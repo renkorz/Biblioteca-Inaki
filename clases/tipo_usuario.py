6dccb7b47d7a4fd74b71893c6504979ad05b53a9
𝑰𝒏̃𝒂𝒌𝒊 𝒁𝒂́𝒓𝒂𝒕𝒆 愛 <zaarate7ignacio@gmail.com>
2024-10-02 21:44:40 -0300

    INTRODUCCION DE ARCHIVOS MANUALES, CAMBIO Y CREACION DE CARPETA NUEVA, AVANCES POR

---
+++ clases/tipo_usuario.py
@@ -0,0 +1,6 @@
+class Tipo_Usuario():
+    def __init__(self, id_tipo_usuario, tipo_usuario, descripcion_tipo_usuario):
+        self.id_tipo_usuario = id_tipo_usuario
+        self.tipo_suuario = tipo_usuario
+        self.descripcion_tipo_usuario = descripcion_tipo_usuario
+        
