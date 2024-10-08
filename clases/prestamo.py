b29bdef3a86108332831c915bcd2b6b23141eef8
𝑰𝒏̃𝒂𝒌𝒊 𝒁𝒂́𝒓𝒂𝒕𝒆 愛 <zaarate7ignacio@gmail.com>
2024-10-08 15:47:17 -0300

    Cambios en variables mal escritas. Errores corregidos.

--- clases/prestamo.py
+++ clases/prestamo.py
@@ -6,7 +6,7 @@ import usuario
 class Prestamo(detalle_libro, usuario):
     def __init__(self, id_prestamo, isbn, id_usuario, fecha_prestamo, fecha_devolucion, fecha_devuelto, cantidad_solicitada):
         detalle_libro.__init__(isbn)
-        usuario.__init__(usuario)
+        usuario.__init__(id_usuario)
         self.id_prestamo = id_prestamo
         self.fecha_prestamo = fecha_prestamo
         self.fecha_devolucion = fecha_devolucion
