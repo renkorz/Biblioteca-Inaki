ecca172719c9bd4f23e6483292ae59790353a253
𝑰𝒏̃𝒂𝒌𝒊 𝒁𝒂́𝒓𝒂𝒕𝒆 愛 <zaarate7ignacio@gmail.com>
2024-10-17 00:08:18 -0300

    .

--- DAL/db2.py
+++ DAL/db2.py
@@ -17,3 +17,25 @@ for filas in cur:
 
 
 cnx.close()
+
+import mysql.connector
+
+# Connect to server
+cnx = mysql.connector.connect(
+    host="127.0.0.1",
+    port=3306,
+    user="mike",
+    password="s3cre3t!")
+
+# Get a cursor
+cur = cnx.cursor()
+
+# Execute a query
+cur.execute("SELECT CURDATE()")
+
+# Fetch one result
+row = cur.fetchone()
+print("Current date is: {0}".format(row[0]))
+
+# Close connection
+cnx.close()
